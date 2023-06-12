14fd1ef29aeaf9dcc5ea3df5c42e75e032008145
rafael-takano <rafaelkuhntakano@hotmail.com>
2023-06-11 23:36:11 -0300

    Fixed userAccess CSS

--- src/components/userAccess/CreateCustomer.vue
+++ src/components/userAccess/CreateCustomer.vue
@@ -32,65 +32,50 @@ export default {
 
 <style scoped>
 
-.Login {
-	position: fixed;
-	z-index: 0;
-	right: 0;   
-	top: 4.43vw;
-	background-color: #fff;
-	height: calc(100vh - 4.43vw);
-	width: 26.042vw;
-	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-	box-shadow: inset 0 2px 2px rgba(0, 0, 0, 0.25);	
-    border-left: 1px #999 solid;
-    padding: 1vw;
-    overflow: scroll;
-
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	text-align: center;
+img {	
+	width: 40%;
+	margin: 10% 30% ;
+	background-color: #fff;	    
 }
 
-.Login p {		
-	font-size: 3vh;
-	color: #999;
+input {
+	display: block;
+	width: 100%;	
+	border: 1px #999 solid;	
+	border-radius: 8px;
+	margin: .3vw 0;
+	padding: 10px;
 }
 
-.Login img {	
-	position: center;
-	padding: 10vh 0 5vh 0;
-	background-color: #fff;
-	max-width: 80%;
-    height: auto;
-}
 
 .button {
-	text-align: center;
-	vertical-align: middle;
+	text-align: center;	
 	line-height: 4.16666vw;
-	width: 90%;
-	height: 4.16666vw;
+	width: 100%;
+	height: auto;
 	border-radius: 8px;
 	border: 2px solid #720000;
-	margin: 2vh .78125vw 0 .78125vw;
 	text-transform: uppercase;
 	background-color: #c92828;
 	font-size: 2vw;
 	color: #fff;
 	font-weight: bold;
+	margin: .3vw 0;
 	-webkit-text-stroke: #720000 1.5px;
 	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
 	cursor: pointer;
 }
 
 @media (max-width: 1280px) {
-    .cartDiv {
-        top: 45px;
-        border: none;
-        height: calc(100% - 45px - 38.3vw);
-        width: 100%;
-    }    
+    input {
+		margin: 1vw 0;
+	}
+
+	.button {
+		margin: 2vw 0;
+		line-height: 15vw;
+		font-size: 8vw;
+	}
 }
 
 </style>
