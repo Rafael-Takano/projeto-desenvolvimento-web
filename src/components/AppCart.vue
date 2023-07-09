d99295d57b2f4246a3f4d2131d12f2b540572e8e
rafael-takano <rafaelkuhntakano@hotmail.com>
2023-07-08 22:45:43 -0300

    Added frontend request to buy

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -1,60 +1,81 @@
-<script setup>    
-    import { ref, toRef, watch , onMounted} from 'vue'
-    import CartItem from './cart/CartItem.vue';
-    import CartBottom from './cart/CartBottom.vue'
-    
-    const emit = defineEmits(['removeItem','confirmPurchase'])
-    const props = defineProps({
-        items: Array,
-    })
+<script setup>
+import { ref, toRef, watch, onMounted } from 'vue'
+import CartItem from './cart/CartItem.vue';
+import CartBottom from './cart/CartBottom.vue'
 
-    const items = toRef(props.items)    
-    const subtotal = ref(0)
-    
-    onMounted(() => {
-        update()
-    })
-    watch(props, update)
+const emit = defineEmits(['removeItem', 'confirmPurchase'])
+const props = defineProps({
+    items: Array,
+})
 
-    function update() {      
-        let aux = 0;
-        props.items.forEach(e => aux += e.price*e.qtd)        
-        subtotal.value = aux
+const items = toRef(props.items)
+const subtotal = ref(0)
+
+onMounted(() => {
+    update()
+})
+watch(props, update)
+
+function update() {
+    let aux = 0;
+    props.items.forEach(e => aux += e.price * e.qtd)
+    subtotal.value = aux
+}
+function remove(item) {
+    emit('removeItem', item);
+}
+
+function tryPurchase(cardData) {
+    if (Object.keys(cardData).some(k => cardData[k] == '')) {
+        alert('There are missing informations of the card');
+        return;
     }
-    function remove(item) {
-        emit('removeItem',item);
+    let data = cardData;
+    data.products = JSON.parse(JSON.stringify(items.value))
+    if (data.products.length == 0) {
+        alert('There is no Products on the shopping cart');
+        return;
     }
-    
+    fetch('/buy', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }).then(async res => console.log(await res.json())
+    ).catch( err => console.log(err))
+}
+
 </script>
 
 <template>
     <div class="cartDiv">
-        <p>Shopping Cart</p>                            
-        <CartItem v-for="(item, index) in items" :item="item" :index="index" @updateSubtotal="update" @removeItem="remove"/>                       
+        <p>Shopping Cart</p>
+        <CartItem v-for="(item, index) in items" :item="item" :index="index" @updateSubtotal="update"
+            @removeItem="remove" />
     </div>
-        <CartBottom :subtotal="subtotal" @confirmPurchase="emit('confirmPurchase')"/>        
+    <CartBottom :subtotal="subtotal" @confirmPurchase="tryPurchase" />
 </template>
 
 <style scoped>
-
 .cartDiv {
-	position: fixed;
-	z-index: 0;
-	right: 0;   
-	top: 4.43vw;
-	background-color: #fff;
-	height: calc(100vh - 4.43vw - 12.7vw);
-	width: 26.042vw;
-	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-	box-shadow: inset 0 2px 2px rgba(0, 0, 0, 0.25);	
+    position: fixed;
+    z-index: 0;
+    right: 0;
+    top: 4.43vw;
+    background-color: #fff;
+    height: calc(100vh - 4.43vw - 12.7vw);
+    width: 26.042vw;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+    box-shadow: inset 0 2px 2px rgba(0, 0, 0, 0.25);
     border-left: 1px #999 solid;
     padding: 1vw;
     overflow: scroll;
 }
 
-.cartDiv p {		
-	font-size: 3vh;
-	color: #999;
+.cartDiv p {
+    font-size: 3vh;
+    color: #999;
 }
 
 @media (max-width: 1280px) {
@@ -63,7 +84,6 @@
         border: none;
         height: calc(100% - 45px - 38.3vw);
         width: 100%;
-    }    
+    }
 }
-
 </style>
--- src/components/cart/CartBottom.vue
+++ src/components/cart/CartBottom.vue
@@ -1,114 +1,170 @@
 <script setup>
-    const props = defineProps({
-        subtotal: Number
-    });
+const props = defineProps({
+    subtotal: Number
+});
+let creditCard = {
+    owner: '',
+    cardNumber: '',
+    CVV: '',
+    monthExp: '',
+    yearExp: ''
+}
 
-    const emit = defineEmits(['confirmPurchase'])
+const emit = defineEmits(['confirmPurchase'])
 
-    function price() {        
-		return props.subtotal.toFixed(2);
-    }	
+function price() {
+    return props.subtotal.toFixed(2);
+}	
 </script>
 
 <template>
     <div class="cart-bottom-div">
-        <p class="subtotal">Subtotal: R$ {{price()}}</p><br>
-        <p class="payment-text">Credit card or Debit card:</p>
-        <input type="text" name="CardNumber" id="cardNumber">
-        <div class="confirm-purchase" @click="emit('confirmPurchase')">Confirm Purchase</div>
+        <p class="subtotal">Subtotal: R$ {{ price() }}</p><br>
+        <p class="payment-text">Owner:</p>
+        <input type="text" name="CardNumber" id="cardNumber" class="Owner" v-model="creditCard.owner">
+        <p class="payment-text">Credit card Number:</p>
+        <input type="text" name="CardNumber" id="cardNumber" class="Owner" v-model="creditCard.cardNumber">
+        <div class="inl-blk">
+            <p class="payment-text" style="display: block;">CVV: </p> <br>
+            <input type="text" name="CardNumber" id="cardNumber" class="CVV" v-model="creditCard.CVV">
+        </div>
+        <div class="inl-blk" style="float: right;">
+            <p class="payment-text" style="width: 10vw; display: block;">ExpirationDate: </p> <br>
+            <input type="text" name="CardNumber" id="cardNumber" class="CVV" style="float: right;" v-model="creditCard.yearExp">
+            <select name="Month" id="cardNumber" class="CVV" style="float: right;" v-model="creditCard.monthExp">
+                <option v-for="mes in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]" :value="mes">{{ mes }}</option>
+            </select>
+        </div>
+        <div class="confirm-purchase" @click="emit('confirmPurchase', creditCard)">Confirm Purchase</div>
     </div>
 </template>
 
 <style scoped>
 .cart-bottom-div {
-	position: fixed;
-	right: 0;
-	bottom: 0px;
-	height: 12.7vw;
-	width: 26.0416vw;
-    padding: 1.04166vw 0;
-    background-color: #fff;    
+    position: fixed;
+    right: 0;
+    bottom: 0px;
+    height: 15.7vw;
+    width: 26.0416vw;
+    padding: 0.4166vw 0;
+    background-color: #fff;
     border: 1px #999 solid
 }
-.subtotal{
+
+.subtotal {
     float: right;
     margin-right: .78125vw;
 }
+
 .payment-text {
     float: left;
     margin-left: .78125vw;
 }
 
-.subtotal, .payment-text{
-	font-size: .9375vw;
-	margin-bottom: .78125vw;
+.subtotal,
+.payment-text {
+    font-size: .7375vw;
+    margin-bottom: .3vw;
     color: #999;
 }
 
-#cardNumber, #cardNumber::selection {
-	line-height: normal;
-	outline: none;
-	text-align: left;
-	width: 24.48vw;
-	margin: 0 .78125vw;
-    font-size: 1.6vw;
-	height: 2vw;
-	background-color: rgba(0, 0, 0, 0);
-	color: #999;
-	border: #999 2px solid;		
-	margin-bottom: .9375vw;
-	cursor: pointer;
+#cardNumber,
+#cardNumber::selection {
+    line-height: normal;
+    outline: none;
+    text-align: left;
+    font-size: .7375vw;
+    height: 1.3vw;
+    background-color: rgba(0, 0, 0, 0);
+    color: #999;
+    border: #999 2px solid;
+    margin-bottom: .2375vw;
+    cursor: pointer;
 }
+
 .confirm-purchase {
-	text-align: center;
-	vertical-align: middle;
-	line-height: 4.16666vw;
-	width: 24.48vw;
-	height: 4.16666vw;
-	border-radius: 8px;
-	border: 2px solid #720000;
-	margin: 0 .78125vw;
-	text-transform: uppercase;
-	background-color: #c92828;
-	font-size: 2vw;
-	color: #fff;
-	font-weight: bold;
-	-webkit-text-stroke: #720000 1.5px;
-	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-	cursor: pointer;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 4.16666vw;
+    width: 24.48vw;
+    height: 4.16666vw;
+    border-radius: 8px;
+    border: 2px solid #720000;
+    margin: 0 .78125vw;
+    text-transform: uppercase;
+    background-color: #c92828;
+    font-size: 2vw;
+    color: #fff;
+    font-weight: bold;
+    -webkit-text-stroke: #720000 1.5px;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+}
+
+.inl-blk {
+    display: inline-block;
+    margin-right: .78125vw;
+
+}
+
+.Owner {
+    margin: 0 .78125vw;
+    width: 24.48vw;
+}
+
+.CVV {
+    margin: 0 0 0 .78125vw;
+    display: block;
+    width: 5vw;
+}
+
+br {
+    height: 0;
 }
 
 @media (max-width: 1280px) {
-    .cart-bottom-div{        
+    .cart-bottom-div {
         width: 100%;
-        height: 38.3vw;
+        height: 48.3vw;
         border: none;
         border-top: 1px #999 solid;
         padding: 1vw;
     }
 
     .subtotal {
-        font-size: 4vw;
+        font-size: 3vw;
         float: none;
         text-align: right;
     }
-    .payment-text{
-        font-size: 3vw;
+
+    .payment-text {
+        font-size: 2.5vw;
         float: none;
     }
 
     #cardNumber {
-        width: 100%;
+
         margin: 0;
-        height: 7vw;
-        font-size: 7vw;
+        height: 4vw;
+        font-size: 3vw;
     }
+
     .confirm-purchase {
         line-height: 15vw;
         width: 100%;
         margin: 2vw 0 0;
         height: 15vw;
-        font-size: 8vw;        
+        font-size: 8vw;
     }
-}
-</style>
+
+    .Owner {
+        width: 100%;
+        margin: 0 .78125vw;
+    }
+
+    .CVV {
+        margin: 0 0 0 .78125vw;
+        display: block;
+        width: 30vw;
+    }
+}</style>
